c242ccf99f4336accc17bdc19f8182c4e16d5fb0
ParrotGaming <killian@onyxwear.com>
2022-05-21 20:37:50 -0700

    Working on implementing roll detection

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,22 +1,30 @@
 const ConnectedDice = {}
+const RollStates = {}
+
+dice_rolled = 0;
+roll_num = 0;
 
 function openConnectionDialog() {
 	const newDice = new GoDice();
 	newDice.requestDevice();
 }
 
+function calculateRoll() {
+    dice_rolled += 1
+}
+
 GoDice.prototype.onDiceConnected = (diceId, diceInstance) => {
     console.log("Dice Connected: " + diceId);
     ConnectedDice[diceId] = diceInstance;
+    RollStates[diceId] = [0, false];
 }
 
 GoDice.prototype.onRollStart = (diceId) => {
-    console.log(diceId + " Is Rolling...")
+    console.log(diceId + " Is Rolling...");
 }
 
 GoDice.prototype.onStable = (diceId, value, xyzArray) => {
-    console.log(diceId + " Rolled A: " +value)
-    if (value == 6) {
-        ConnectedDice[diceId].setLed([0,0,255], [255,0,0]);
-    }
+    console.log(diceId + " Rolled A: " + value);
+    RollStates[diceId] = [value, false];
+    calculateRoll();
 }
